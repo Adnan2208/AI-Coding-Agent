31e06a692e26559c3275430292651c00decc4197
Adnan <adnan.c2208@gmail.com>
2026-02-04 23:34:31 +0530

    Dynamic prompting enabled

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import sys
 
 from groq import Groq
 from dotenv import load_dotenv
@@ -8,6 +9,13 @@ client = Groq(
     api_key=os.environ.get("GROQ_API_KEY"),
 )
 
+if(len(sys.argv)) < 2:
+    print("Please enter a prompt")
+    sys.exit(1)
+else :
+    UserPrompt =  str(sys.argv[1])
+
+
 chat_completion = client.chat.completions.create(
     messages=[
         {
@@ -16,10 +24,10 @@ chat_completion = client.chat.completions.create(
         },
         {
             "role": "user",
-            "content": "Explain the importance of fast language models",
+            "content": UserPrompt,
         }
     ],
-    model="llama-3.3-70b-versatile",
+    model="llama-3.1-8b-instant",
 )
 
 print(chat_completion.choices[0].message.content)
