163e896e83e935dcbcc8488470a0fc94609a6345
Adnan <adnan.c2208@gmail.com>
2026-02-20 18:19:00 +0530

    Test updated

--- test.py
+++ test.py
@@ -4,6 +4,8 @@ def main():
 
     # Fix both logic in get_files_info()
     print(get_files_info("calculator","."))
-    print(get_files_info("calculator","Ai-agent"))
+    print(get_files_info("calculator","pkg"))
+    print(get_files_info("calculator","../"))
+    print(get_files_info("calculator","bin"))
 
 main()
